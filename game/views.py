83d5616590d875aa8d897410f0c2c0c5489fe0ce
Maxi Redigonda <maxiredigonda@gmail.com>
2018-09-15 00:07:18 -0300

    Connected home url to mocked view with resources data

--- game/views.py
+++ game/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from django.views.generic.base import TemplateView
+
 # Create your views here.
+class HomeView(TemplateView):
+    template_name = 'game/home.html'
+
+    def get_context_data(self):
+        context = {}
+
+        context['gold_units'] = 14
+        context['gold_subunits'] = 763
+        context['rock_units'] = 26
+        context['rock_subunits'] = 144
+        context['wood_units'] = 32
+        context['wood_subunits'] = 298
+
+        return context
