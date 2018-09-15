bd15b2dfea6a0b3879b64ded466929c66cf2b454
Maxi Redigonda <maxiredigonda@gmail.com>
2018-09-15 11:01:25 -0300

    Created Resource model

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import Resource
+
+admin.site.register(Resource)
--- game/models.py
+++ game/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Resource(models.Model):
+    gold = models.PositiveIntegerField()
+    gold_production = models.PositiveIntegerField()
+    rock = models.PositiveIntegerField()
+    rock_production = models.PositiveIntegerField()
+    wood = models.PositiveIntegerField()
+    wood_production = models.PositiveIntegerField()
+    last_updated = models.DateTimeField()
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
